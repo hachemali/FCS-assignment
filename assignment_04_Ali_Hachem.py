597a302387824d9e726a5e5ac4fd77ef514dfd86
El_Siyyed <hachemali123@gmail.com>
2024-03-30 14:38:27 +0200

    task id getter and setter

--- assignment_04_Ali_Hachem.py
+++ assignment_04_Ali_Hachem.py
@@ -1,6 +1,6 @@
 class Task:
-    def __init__(self,task_id,descritpion,priority,completed):
-        self.task_id=task_id
-        self.description=descritpion
-        self.priority=priority
-        self.completed=completed
+    def __init__(self,__task_id,__descritpion,__priority,__completed):
+        self.__task_id=__task_id
+        self.__description=__descritpion
+        self.__priority=__priority
+        self.__completed=__completed
