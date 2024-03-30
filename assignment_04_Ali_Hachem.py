10cb4105252e538e85e6bd7fd7b00010bd70ef7e
El_Siyyed <hachemali123@gmail.com>
2024-03-30 11:42:11 +0200

    added task class

---
+++ assignment_04_Ali_Hachem.py
@@ -0,0 +1,6 @@
+class Task:
+    def __init__(self,task_id,descritpion,priority,completed):
+        self.task_id=task_id
+        self.description=descritpion
+        self.priority=priority
+        self.completed=completed
